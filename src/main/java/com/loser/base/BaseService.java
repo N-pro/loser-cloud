1a9399389f7abde222332134e5c76f01aa535835
hezhixiong <jason_hzx@126.com>
2020-06-12 10:29:14 +0800

    baseUtil update

--- src/main/java/com/loser/base/BaseService.java
+++ src/main/java/com/loser/base/BaseService.java
@@ -19,94 +19,9 @@ import java.util.Map;
  * @author Jason
  * @version createTime 2020/6/11 23:31
  */
-public class BaseService<T extends BasedEntity> implements BaseMapper {
+public class BaseService<T extends BasedEntity> {
 
     @Autowired
     BaseMapper<T> mapper;
 
-
-    @Override
-    public int insert(Object entity) {
-        return 0;
-    }
-
-    @Override
-    public int deleteById(Serializable id) {
-        return 0;
-    }
-
-    @Override
-    public int delete(Wrapper wrapper) {
-        return 0;
-    }
-
-    @Override
-    public int updateById(Object entity) {
-        return 0;
-    }
-
-    @Override
-    public int update(Object entity, Wrapper updateWrapper) {
-        return 0;
-    }
-
-    @Override
-    public Object selectById(Serializable id) {
-        return null;
-    }
-
-    @Override
-    public Object selectOne(Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public Integer selectCount(Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public List selectList(Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public List<Map<String, Object>> selectMaps(Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public List<Object> selectObjs(Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public IPage<Map<String, Object>> selectMapsPage(IPage page, Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public IPage selectPage(IPage page, Wrapper queryWrapper) {
-        return null;
-    }
-
-    @Override
-    public List selectByMap(Map columnMap) {
-        return null;
-    }
-
-    @Override
-    public List selectBatchIds(Collection idList) {
-        return null;
-    }
-
-    @Override
-    public int deleteBatchIds(Collection idList) {
-        return 0;
-    }
-
-    @Override
-    public int deleteByMap(Map columnMap) {
-        return 0;
-    }
 }
